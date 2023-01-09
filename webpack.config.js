5c04555776cc9116fe81e3eb173e4c75d2c88846
Viktoryia <viktoriabakun00@gmail.com>
2023-01-09 19:18:18 +0300

    feat(app): add static favicon (#2)

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,31 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.tsx',
-    devtool: 'inline-source-map',
-    mode: "development",
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Output Management',
-            template: "src/index.html"
-        }),
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+      template: "src/index.html",
+      favicon: "public/favicon.ico",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
