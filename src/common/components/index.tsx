0d5f1252d863c132caa15201144781bbcd2df36f
Viktoryia <viktoriabakun00@gmail.com>
2023-01-09 14:59:18 +0100

    feat(folders): add folder structure

---
+++ src/common/components/index.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import React from "react";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const TitleComponent = () => <Title>Styled component title</Title>;
+
+export default TitleComponent;
