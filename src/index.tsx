963f889a3eeb10bdeed6d77e5478931940ac5461
Viktoryia <viktoriabakun00@gmail.com>
2023-01-09 17:12:41 +0300

    feat(folders): add folder structure (#1)

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,14 @@
-import React from "react";
-import { FC } from "react";
+import React, { FC } from "react";
 
 import { createRoot } from "react-dom/client";
-import styled from "styled-components";
+import TitleComponent from "./common/components";
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
 const App: FC = () => {
   const name = "Viktoryia";
 
   return (
     <div>
-      <Title>Styled component title</Title>
+      <TitleComponent />
       hello there,
       {name}
     </div>
